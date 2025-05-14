ae813b911bfc1bf70f26648754f1c5c052294e3a
QACPERO <kacperd793@gmail.com>
2025-05-14 16:43:23 +0200

    Update zadanie7_square.cpp

--- zajecia1/zadanie7_square.cpp
+++ zajecia1/zadanie7_square.cpp
@@ -1,3 +1,29 @@
+//zadanie czesc 2 z przkatnymi 
+#include <iostream>
+
+using namespace std;
+
+void draw_square(const int &count, const bool &left_diagonal, const bool &right_diagonal){
+    for (int i = 0; i < count; i++){
+        for (int j = 0; j <count; j++){
+            if(j==0 || i == 0 || i == count -1 || j == count -1) cout << "#";
+            else if(left_diagonal == true && i == j) cout << "#";
+            else if(right_diagonal == true && i + j == count-1) cout << "#";
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    draw_square(7, true, true);
+    return 0;
+}
+
+
+//================================================================================================
+//zadanie czesc 1
 #include <iostream>
 
 using namespace std;
