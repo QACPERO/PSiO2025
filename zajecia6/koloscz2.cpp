122d362af2b5879dd4c3a5c0b8186d311ab1f338
QACPERO <kacperd793@gmail.com>
2025-05-19 12:29:18 +0200

    Update koloscz2.cpp

--- zajecia6/koloscz2.cpp
+++ zajecia6/koloscz2.cpp
@@ -3,41 +3,46 @@
 #include <algorithm>
 #include <memory>
 
-                             using namespace std;
+using namespace std;
 
 class Pojazd {
 public:
-    virtual ~Pojazd() = default;
-
-    string nazwa;
-    int miejsca;
-    float pojemnosc;
-    Pojazd(){};
-    Pojazd(string n, float p, int m) : nazwa(n), miejsca(m), pojemnosc(p) {}
-    Pojazd(string n, float p) :  nazwa(n), pojemnosc(p) {}
-    virtual string toString()= 0;
+    virtual string toString() = 0;
+    virtual string getName() = 0;
+    virtual int wheels() = 0;
+    virtual double weight() = 0;
+    virtual ~Pojazd() {}
 };
+
 class Auto : public Pojazd {
+    string name;
+    double waga;
+    int siedzenia;
 public:
-    Auto(string n, float p, int m) : Pojazd(n,p,m) {}
-
-    virtual string toString(){
-        return "Typ: Auto, Nazwa: " + nazwa + ", Waga: " + to_string(pojemnosc) + ", kola: 4," + "siedzienia" + to_string(miejsca);
+    Auto(string n, double w, int s) : name(n), waga(w), siedzenia(s) {}
+    string toString() {
+        return "Typ: Auto, Nazwa: " + name + ", Waga: " + to_string(waga) + ", Kola: 4, Siedzenia: " + to_string(siedzenia);
     }
+    string getName() { return name; }
+    int wheels() { return 4; }
+    double weight() { return waga; }
 };
+
 class Motocykl : public Pojazd {
+    string name;
+    double waga;
 public:
-    Motocykl(string n, float p, int m) : Pojazd(n,p,m) {}
-    Motocykl(string n , float p) : Pojazd(n,p) {}
-
-    virtual string toString(){
-        return "Typ: Motocykl, Nazwa: " + nazwa + ", Waga: " + to_string(pojemnosc) + ", kola: 2," + "siedzienia" + to_string(miejsca);
+    Motocykl(string n, double w) : name(n), waga(w) {}
+    string toString() {
+        return "Typ: Motocykl, Nazwa: " + name + ", Waga: " + to_string(waga) + ", Kola: 2";
     }
+    string getName() { return name; }
+    int wheels() { return 2; }
+    double weight() { return waga; }
 };
 
-ostream &operator<<(ostream &os, Pojazd &p){
-    os << p.toString();
-    return os;
+ostream& operator<<(ostream& os, Pojazd& p) {
+    return os << p.toString();
 }
 
 int main() {
@@ -50,11 +55,11 @@ int main() {
     // Uzupelnij implementacje klasy Pojazd i wyprowadzonych klas Auto i Motocykl
     // tak aby uruchomic poprawnie ponizszy kod
     // PUNKTY: 14
-    using Pojazdy = std::vector<std::unique_ptr<Pojazd>>;
+    using Pojazdy = vector<unique_ptr<Pojazd>>;
     Pojazdy pojazdy;
-    pojazdy.emplace_back(std::make_unique<Auto>(Auto("Toyota Land Cruiser", 2500.0, 7)));
-    pojazdy.emplace_back(std::make_unique<Auto>(Auto("Chevrolet Camaro v8", 1700.0, 4)));
-    pojazdy.emplace_back(std::make_unique<Motocykl>(Motocykl("Ducati Panigale V4", 175.0)));
+    pojazdy.emplace_back(make_unique<Auto>("Toyota Land Cruiser", 2500.0, 7));
+    pojazdy.emplace_back(make_unique<Auto>("Chevrolet Camaro v8", 1700.0, 4));
+    pojazdy.emplace_back(make_unique<Motocykl>("Ducati Panigale V4", 175.0));
 
     // Skonstruuj operator wstawienia do strumienia tak aby obiekty byly wyswietlane jak ponizej:
     // Podpowiedz: dodaj i uzyj w operatorze metody wirtualnej toString() (Lab05.1.cpp)
@@ -62,12 +67,9 @@ int main() {
     // Typ: Auto, Nazwa: Chevrolet Camaro v8, Waga: 1700.000000, Kola: 4, Siedzenia: 4
     // Typ: Motocykl, Nazwa: Ducati Panigale V4, Waga: 175.000000, Kola: 2
     // PUNKTY: 5
-
-    for (auto& p: pojazdy)
+    for (auto& p : pojazdy)
         cout << *p << endl;
-
-    cout << "===============================" << endl;
-
+    cout << "=======================" << endl;
     // Posortuj obiekty wg nazwy (nie wg typu!)
     // Podpowiedz: dodaj i uzyj metody wirtualnej getName()
     // PUNKTY: 5
@@ -75,12 +77,12 @@ int main() {
     // Typ: Auto, Nazwa: Chevrolet Camaro v8, Waga: 1700.000000, Kola: 4, Siedzenia: 4
     // Typ: Motocykl, Nazwa: Ducati Panigale V4, Waga: 175.000000, Kola: 2
     // Typ: Auto, Nazwa: Toyota Land Cruiser, Waga: 2500.000000, Kola: 4, Siedzenia: 7
-    std::sort(pojazdy.begin(), pojazdy.end(), [](std::unique_ptr<Pojazd> &p1, std::unique_ptr<Pojazd> &p2){return p1->nazwa < p2->nazwa;});
-
-    for (auto& p: pojazdy)
+    sort(pojazdy.begin(), pojazdy.end(), [](auto& a, auto& b) {
+        return a->getName() < b->getName();
+    });
+    for (auto& p : pojazdy)
         cout << *p << endl;
-    cout << "===============================" << endl;
-
+    cout << "=======================" << endl;
     // Obiekty posortowane wg malejacej liczby kol i rosnacej wagi.
     // Podpowiedz: dodaj i uzyj dodatkowych metod wirtualnych wheels() i weight()
     // PUNKTY: 8
@@ -88,22 +90,13 @@ int main() {
     // Typ: Auto, Nazwa: Chevrolet Camaro v8, Waga: 1700.000000, Kola: 4, Siedzenia: 4
     // Typ: Auto, Nazwa: Toyota Land Cruiser, Waga: 2500.000000, Kola: 4, Siedzenia: 7
     // Typ: Motocykl, Nazwa: Ducati Panigale V4, Waga: 175.000000, Kola: 2
-    std::sort(pojazdy.begin(), pojazdy.end(), [](std::unique_ptr<Pojazd> &p1, std::unique_ptr<Pojazd> &p2){
-        if(p1->miejsca>p2->miejsca){
-            return true;
-        }
-        else if(p1->miejsca<p2->miejsca){
-            return false;
-        }
-        else{
-            if(p1->pojemnosc > p2->pojemnosc) return true;
-            else return false;
-        }
+    sort(pojazdy.begin(), pojazdy.end(), [](auto& a, auto& b) {
+        if (a->wheels() != b->wheels())
+            return a->wheels() > b->wheels();
+        return a->weight() < b->weight();
     });
-
-    for (auto& p: pojazdy)
+    for (auto& p : pojazdy)
         cout << *p << endl;
-    cout << "===============================" << endl;
 
     return 0;
 }
